a9d3c684153aaeb9839f89a2982591b56773774d
sanchex-22 <david21.cs70@gmail.com>
2024-07-03 16:48:39 -0500

    m

--- src/components/ui_components/footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    const date = new Date()
-    return(
-        <nav className="bg-white w-full h-12 flex justify-center items-center px-4">
-            <p>CopyRight {date.getFullYear()}</p>
-        </nav>
-    )
-}
