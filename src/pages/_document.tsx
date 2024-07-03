4c917aae1b7095c80f6c8bd5dff00d8b64809d7f
sanchex-22 <david21.cs70@gmail.com>
2024-07-03 14:50:15 -0500

    0

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Navbar from "../components/ui_components/navbar";
+import Footer from "@/components/ui_components/footer";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-slate-100">
         <Main />
         <NextScript />
       </body>
